556e9d0036e142cfa3666ed996fbdc78d2e38479
Nguyen Khanh Duy <ricky.ngk@gmail.com>
2015-09-24 19:38:22 +0700

    user now can post & view comments

--- web/src/app/partials/idea_feed/idea_feed.js
+++ web/src/app/partials/idea_feed/idea_feed.js
@@ -7,38 +7,42 @@ angular.module('inspinia')
             group: '@'
         },
         controller: function($scope, firebaseHelper, $sce, $rootScope) {
-            var date = new Date($scope.data.createdDate)
-            var year = date.getFullYear();
-            var month = date.getMonth() + 1;
-            var day = date.getDate();
-            var hour = date.getHours();
-            var minute = date.getMinutes();
-            var sec = date.getSeconds();
-            if (month < 10) {month = "0" + month};
-            if (day < 10) {day = "0" + day};
-            if (hour < 10) {hour = "0" + hour};
-            if (minute < 10) {minute = "0" + minute};
-            if (sec < 10) {sec = "0" + sec};
-            $scope.createdDate = year + "/" + month + "/" + day + " " + hour + ":" + minute + ":" + sec;
+            $scope.formatDate = function(input) {
+                var date = new Date(input)
+                var year = date.getFullYear();
+                var month = date.getMonth() + 1;
+                var day = date.getDate();
+                var hour = date.getHours();
+                var minute = date.getMinutes();
+                var sec = date.getSeconds();
+                if (month < 10) {month = "0" + month};
+                if (day < 10) {day = "0" + day};
+                if (hour < 10) {hour = "0" + hour};
+                if (minute < 10) {minute = "0" + minute};
+                if (sec < 10) {sec = "0" + sec};
+                return year + "/" + month + "/" + day + " " + hour + ":" + minute + ":" + sec;
+            }
+            $scope.createdDate = $scope.formatDate($scope.data.createdDate)
 
             $scope.$watch("data.title", function() {
                 if ($scope.data.title) {
                     $scope.title = $sce.trustAsHtml($scope.data.title.replace(/(?:\r\n|\r|\n)/g, '<br />'));
                 }
             })
-            // $scope.title = $sce.trustAsHtml($scope.data.title.replace(/(?:\r\n|\r|\n)/g, '<br />'));
 
             $scope.user_profile = firebaseHelper.syncObject("profiles_pub/" + $scope.data.uid);
+            $scope.me = firebaseHelper.syncObject("profiles_pub/" + firebaseHelper.getUID());
             $scope.created_user_profile = firebaseHelper.syncObject("profiles_pub/" + $scope.data.createdBy);
             $scope.gravatar = $rootScope.gravatar;
-
-            // $scope.ideas_stat = firebaseHelper.syncObject(["ideas", $scope.group, $scope.data.$id, "stat"]);
             $scope.ideas_vote = firebaseHelper.syncObject(["ideas_votes", $scope.group, $scope.data.$id, firebaseHelper.getUID()]);
+
+            $scope.$on("user:login", function(data) {
+                $scope.me = firebaseHelper.syncObject("profiles_pub/" + firebaseHelper.getUID());
+            });
 
             var upvote = function(from_down_vote) {
                 firebaseHelper.transaction(["ideas", $scope.group, $scope.data.$id], function(data) {
                     if (!data) {
-                        //data = {up_votes: 0, down_votes: 0, comments: 0, score: 0};
                         return;
                     }
                     data.up_votes = (data.up_votes || 0) + 1;
@@ -52,7 +56,6 @@ angular.module('inspinia')
             var downvote = function(from_up_vote) {
                 firebaseHelper.transaction(["ideas", $scope.group, $scope.data.$id], function(data) {
                     if (!data) {
-                        // data = {up_votes: 0, down_votes: 0, comments: 0, score: 0};
                         return;
                     }
                     data.down_votes = (data.down_votes || 0) + 1;
@@ -66,7 +69,6 @@ angular.module('inspinia')
             var unvote = function(from_up_vote, from_down_vote) {
                 firebaseHelper.transaction(["ideas", $scope.group, $scope.data.$id], function(data) {
                     if (!data) {
-                        // data = {up_votes: 0, down_votes: 0, comments: 0, score: 0};
                         return;
                     }
                     if (from_up_vote) {
@@ -76,6 +78,16 @@ angular.module('inspinia')
                         data.down_votes = (data.down_votes || 0) - 1;
                     }
                     data.score = (data.up_votes || 0) - (data.down_votes || 0);
+                    return data;
+                })
+            }
+
+            $scope.IncrComment = function() {
+                firebaseHelper.transaction(["ideas", $scope.group, $scope.data.$id], function(data) {
+                    if (!data) {
+                        return;
+                    }
+                    data.comments = (data.comments || 0) + 1;
                     return data;
                 })
             }
@@ -124,8 +136,36 @@ angular.module('inspinia')
                     return data;
                 });
             }
+
+            $scope.showComments = false;
             $scope.onComments = function() {
-                $rootScope.notifyError("This feature is not implemented yet")
+                $scope.showComments = !$scope.showComments;
+
+                if ($scope.showComments && !$scope.comments_ref) {
+                    $scope.comments_ref = firebaseHelper.getFireBaseInstance(["idea_comments", $scope.group, $scope.data.$id]);
+                    $scope.comments = firebaseHelper.syncArray($scope.comments_ref.limitToLast(50));
+                }
+            }
+
+            $scope.comment = "";
+            $scope.onSendComment = function() {
+                var comment = $scope.comment;
+                console.log(comment)
+                if (firebaseHelper.getUID()) {
+                    $scope.comments_ref.push().set({
+                        createdDate: Date.now(),
+                        comment: comment,
+                        uid: firebaseHelper.getUID(),
+                        email: firebaseHelper.getAuthEmail(),
+                        display_name: $scope.me.display_name
+                    }, function(ref) {
+                        $scope.IncrComment();
+                        $scope.comment = "";
+                        $scope.$digest();
+                    })
+                } else {
+                    $rootScope.notifyError("Something wrong @@");
+                }
             }
         },
         templateUrl: "app/partials/idea_feed/idea_feed.html"
