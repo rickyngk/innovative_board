a8a6455241c79aab1f37a1e9204a43e73c18ee5f
Nguyen Khanh Duy <ricky.ngk@gmail.com>
2015-09-25 17:49:27 +0700

    refactor

--- slackbot/ib_bot__new_idea.js
+++ slackbot/ib_bot__new_idea.js
@@ -3,42 +3,51 @@ var keys = require("./keys")();
 var request = require('request');
 var ref = new Firebase("https://" + keys.firebase_app + ".firebaseio.com");
 
-var create_group = function(res) {
-
+var create_group = function(res, share) {
+    return res.status(200).json({text: "Begin create group" + JSON.stringify(share)});
 }
 
-var create_user_profile = function(res, uid, user_email, user_display_bame, user_avatar, slack_id) {
-    ref.child("profiles").child(uid).set({
+var get_user_profile_by_email = function(res, share) {
+    ref.child("profiles_pub").orderByChild("email").startAt(user_email).endAt(user_email).once('value', function(snap) {
+        share.uid = snap.key();
+        create_group(share);
+    }, function() {
+        return res.status(200).json({text: "Error getting user public profile"});
+    })
+}
+
+var create_user_profile = function(res, share) {
+    ref.child("profiles").child(share.uid).set({
         role: "user"
     }, function(error) {
         if (error) {
             return res.status(200).json({text: "Error creating user role"});
         } else {
-            ref.child("profiles_pub").child(uid).set({
-                email: user_email,
-                display_name: user_display_bame,
-                avatar: user_avatar,
-                slack_id: slack_id
+            ref.child("profiles_pub").child(share.uid).set({
+                email: share.user_email,
+                display_name: share.user_display_bame,
+                avatar: share.user_avatar,
+                slack_id: share.slack_id
             }, function(error2) {
                 if (error2) {
                     return res.status(200).json({text: "Error creating user public profile"});
                 } else {
-                    return res.status(200).json({text: "Successfully created user account with uid:" + uid});
+                    create_group(share);
                 }
             })
         }
     })
 }
 
-var create_user = function(res, user_email, user_display_bame, user_avatar, slack_id) {
+var create_user = function(res, share) {
     ref.createUser({
-        email: user_email,
+        email: share.user_email,
         password: Math.round((Math.pow(36, 16 + 1) - Math.random() * Math.pow(36, 16))).toString(36).slice(1) + ""
     }, function(error, userData) {
             if (error) {
                 switch (error.code) {
                     case "EMAIL_TAKEN":
-                        return res.status(200).json({text: "email taken " + JSON.stringify(error)});
+                        get_user_profile_by_email(res, share);
                         break;
                     case "INVALID_EMAIL":
                         return res.status(200).json({text: "Can not create user count. Invalid email"});
@@ -46,7 +55,8 @@ var create_user = function(res, user_email, user_display_bame, user_avatar, slac
                         return res.status(200).json({text: "Error creating user"});
                 }
             } else {
-                create_user_profile(res, userData.uid, user_email, user_display_bame, user_avatar, slack_id);
+                share.uid = userData.uid
+                create_user_profile(res, share);
             }
         }
     );
@@ -65,21 +75,18 @@ module.exports = function (req, res, next) {
     text=googlebot: What is the air-speed velocity of an unladen swallow?
     trigger_word=googlebot:
     */
-
-    var channel_id          = req.body.channel_id;
-    var channel_name        = req.body.channel_name;
-    var user_name           = req.body.user_name;
-    var user_id             = req.body.user_id;
-    var text                = req.body.text;
-    var trigger_text        = req.body.trigger_word;
-    var message             = text.replace(trigger_text, "").trim();
+    var share = {
+        channel_id: req.body.channel_id,
+        channel_name: req.body.channel_name,
+        user_name: req.body.user_name,
+        slack_id: req.body.user_id,
+        text: req.body.text,
+        trigger_word: req.body.trigger_word
+    }
 
     console.log("Request received:", JSON.stringify(req.body));
 
-
-    var do_creat_
-
-    if (user_name === 'slackbot') {
+    if (share.user_name === 'slackbot') {
         return res.status(200).end();
     }
 
@@ -89,7 +96,7 @@ module.exports = function (req, res, next) {
         } else {
             //check if user existed
             request({
-                uri: "https://slack.com/api/users.info?token=" + keys.slack + "&user=" + user_id,
+                uri: "https://slack.com/api/users.info?token=" + keys.slack + "&user=" + share.slack_id,
                 method: 'GET',
             }, function (error, response, body) {
                 if (error) {
@@ -104,27 +111,16 @@ module.exports = function (req, res, next) {
                 if (!obj.ok || !obj.user) {
                     return res.status(200).json({text: "Slack rejected request." + body});
                 }
-                var user_email = obj.user.profile.email;
-                var user_display_name = obj.user.name || obj.user.real_name;
-                var user_avatar =   obj.user.profile.image_72
+                share.user_email = obj.user.profile.email;
+                share.user_display_name = obj.user.name || obj.user.real_name;
+                share.user_avatar =   obj.user.profile.image_72
                                 ||  obj.user.profile.image_48
                                 ||  obj.user.profile.image_192
                                 ||  obj.user.profile.image_32
                                 ||  obj.user.profile.image_24
 
-                create_user(res, user_email, user_display_name, user_avatar, user_id);
+                create_user(res, share);
             });
-
-
-            // ref.child("groups").child("slack_" + channel_id).transaction(function(currentData){
-            //     if (currentData === null) {
-            //         return {
-            //             createdDate: Date.now(),
-            //             type: "slack",
-            //             name: channel_name
-            //         }
-            //     }
-            // })
         }
     });
 }
