194f3491c3cee8a111def157f91835ec648e9e87
Nguyen Khanh Duy <ricky.ngk@gmail.com>
2015-09-28 02:12:46 +0700

    fix login form

--- web/src/app/auth/login.js
+++ web/src/app/auth/login.js
@@ -1,4 +1,4 @@
-angular.module('inspinia').controller('LoginCtrl', function ($scope, $state, firebaseHelper) {
+angular.module('inspinia').controller('LoginCtrl', function ($scope, $state, firebaseHelper, $timeout) {
 	$scope.email = "";
 	$scope.password = "";
 	$scope.isForgotPasswordMode = false;
@@ -7,10 +7,14 @@ angular.module('inspinia').controller('LoginCtrl', function ($scope, $state, fir
 			firebaseHelper.resetPassword($scope.email, {
 				success: function(data) {
 					$scope.isForgotPasswordMode = false;
-					$scope.$apply();
+					$timeout(function() {
+						$scope.$apply();
+					}, 100);
+
 				}
 			});
 		} else {
+			console.log($scope.email, $scope.password);
 			firebaseHelper.login($scope.email, $scope.password, {
 	            success: function(data) {
 	                $state.go("index.main");
