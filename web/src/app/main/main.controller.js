ffe01887a7d691dfd943f6ed6898789436c760fd
Nguyen Khanh Duy <ricky.ngk@gmail.com>
2015-09-28 03:06:21 +0700

    filter idea by groups

--- web/src/app/main/main.controller.js
+++ web/src/app/main/main.controller.js
@@ -2,7 +2,10 @@ angular.module('inspinia')
   .controller('MainCtrl', function ($scope, firebaseHelper, $rootScope) {
         $scope.ideas = null;
         $scope.isLoading = true;
-        $scope.groupID = "-JzxqFZwj34iRvz2dcrk";
+        $scope.groupID = "slack_C0B977PLZ";
+
+        $rootScope.currentGroup = "public";
+        $rootScope.userGroups = [];
 
 
         // $scope.ideas = firebaseHelper.syncArray("ideas");
@@ -13,8 +16,19 @@ angular.module('inspinia')
             $scope.isLoading = false;
         })
 
+        console.log("xxx");
         $scope.$on("user:login", function() {
             firebaseHelper.bindObject("profiles/" + firebaseHelper.getUID(), $scope, "data");
+            firebaseHelper.getFireBaseInstance(["user_group", firebaseHelper.getUID()]).once("value", function(snapshot) {
+                $rootScope.userGroups = [];
+                var groups = snapshot.val();
+                for (k in groups) {
+                    if (groups[k]) {
+                        $rootScope.userGroups.push(k);
+                    }
+                }
+                $rootScope.currentGroup = $rootScope.userGroups[0];
+            });
         })
 
         $scope.showAddBlock = false;
