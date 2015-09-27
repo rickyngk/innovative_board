d3534db23ea8551acfa2a22db008129f5806a82f
Nguyen Khanh Duy <ricky.ngk@gmail.com>
2015-09-28 01:51:39 +0700

    support reset password

--- web/src/app/auth/login.js
+++ web/src/app/auth/login.js
@@ -1,12 +1,22 @@
 angular.module('inspinia').controller('LoginCtrl', function ($scope, $state, firebaseHelper) {
 	$scope.email = "";
 	$scope.password = "";
+	$scope.isForgotPasswordMode = false;
 	$scope.onlogin = function() {
-        firebaseHelper.login($scope.email, $scope.password, {
-            success: function(data) {
-                $state.go("index.main");
-            }
-        });
+		if ($scope.isForgotPasswordMode) {
+			firebaseHelper.resetPassword($scope.email, {
+				success: function(data) {
+					$scope.isForgotPasswordMode = false;
+					$scope.$apply();
+				}
+			});
+		} else {
+			firebaseHelper.login($scope.email, $scope.password, {
+	            success: function(data) {
+	                $state.go("index.main");
+	            }
+	        });
+		}
 		return true;
 	}
 });
