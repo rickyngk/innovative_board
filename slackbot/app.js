d78362dd5d1ee7e25bfaec46f9ce17c992d6e5b1
Nguyen Khanh Duy <ricky.ngk@gmail.com>
2015-09-29 01:10:37 +0700

    use dynamic cors

--- slackbot/app.js
+++ slackbot/app.js
@@ -1,15 +1,18 @@
 var express = require('express');
+var cors = require('cors')
 var bodyParser = require('body-parser');
 var app = express();
 var port = 4000;
 var bot__new_idea = require('./ib_bot__new_idea');
 var bot__notif = require('./ib_bot__notif');
 
-app.all('*', function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "http://localhost:3000, http://iamprogrammer.work:8080, http://www.iamprogrammer.work:8080, https://ib-slack.firebaseapp.com");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-});
+var whitelist = ['http://localhost:3000', 'http://iamprogrammer.work:8080', 'https://ib-slack.firebaseapp.com'];
+var corsOptions = {
+    origin: function(origin, callback){
+        var originIsWhitelisted = whitelist.indexOf(origin) !== -1;
+        callback(null, originIsWhitelisted);
+    }
+};
 
 // body parser middleware
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -17,7 +20,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 // test route
 app.get('/', function (req, res) { res.status(200).send('Hello world!') });
 app.post('/idea', bot__new_idea);
-app.post('/notif', bot__notif);
+app.post('/notif', cors(corsOptions), bot__notif);
 
 // error handler
 app.use(function (err, req, res, next) {
