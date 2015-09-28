de0583965bfbd551b4a9d2ec82657d1179471fb5
Nguyen Khanh Duy <ricky.ngk@gmail.com>
2015-09-29 00:51:35 +0700

    support cors

--- slackbot/app.js
+++ slackbot/app.js
@@ -5,6 +5,12 @@ var port = process.env.PORT || 4000;
 var bot__new_idea = require('./ib_bot__new_idea');
 var bot__notif = require('./ib_bot__notif');
 
+app.all('*', function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "http://localhost:3000, http://iamprogrammer.work:8080, http://www.iamprogrammer.work:8080, https://ib-slack.firebaseapp.com");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
 // body parser middleware
 app.use(bodyParser.urlencoded({ extended: true }));
 
@@ -15,8 +21,6 @@ app.post('/notif', bot__notif);
 
 // error handler
 app.use(function (err, req, res, next) {
-    res.header("Access-Control-Allow-Origin", "http://localhost:3000, http://iamprogrammer.work:8080, http://www.iamprogrammer.work:8080, https://ib-slack.firebaseapp.com");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     if (err) {
         console.error(err.stack);
         res.status(400).send(err.message);
